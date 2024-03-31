cd9fa3830eacdc4de65cabb04b5b40fc20af9c71
famgz <famgz@proton.me>
2024-03-31 13:13:18 -0300

    feat: Initial write mode implementation. Exception handler in all routes.

--- src/main.py
+++ src/main.py
@@ -4,8 +4,7 @@ from config import deputados_path, frentes_path
 
 app = Flask('app')
 
-
-keys = {
+KEYS = {
     'deputados': {
         'path': deputados_path,
         'read': ['id', 'nome', 'siglaPartido', 'siglaUf'],
@@ -21,38 +20,79 @@ keys = {
 
 @app.post('/deputados')
 def create_deputado():
-    return write('deputados', request.form)
+    try:
+        return write('deputados', request.form)
+    except Exception as e:
+        return f'ERRO: {e}'
 
 
 @app.get('/deputados')
 def get_deputados():
-    return read('deputados', request.form)
+    try:
+        return read('deputados', request.form)
+    except Exception as e:
+        print(e)
+        return f'ERRO: {e}'
 
 
 @app.post('/frentes')
 def create_frente():
-    return write('frentes', request.form)
+    try:
+        return write('frentes', request.form)
+    except Exception as e:
+        return f'ERRO: {e}'
 
 
 @app.get('/frentes')
 def get_frentes():
-    return read('frentes', request.form)
+    try:
+        return read('frentes', request.form)
+    except Exception as e:
+        return f'ERRO: {e}'
 
 
-def write(type_):
-    return
+def write(type_, form):
+    path = KEYS[type_]['path']
+    query_keys = KEYS[type_]['write']
+    query = parse_form(form, query_keys)
+    missing_keys = [key for key in query_keys if key not in query]
+    if missing_keys:
+        raise Exception(
+            f'Modo de escrita com parâmetros inválidos ou ausentes: {", ".join(missing_keys)}')
+    items = json_(path)
+    return 'ok'
 
 
 def read(type_, form):
-    path = keys[type_]['path']
+    path = KEYS[type_]['path']
     items = json_(path)
-    print(items)
+    # return all items
     if not form:
         return items
-    keys_ = keys[type_]['read']
-    query = parse_form(form, keys_)
+    query_keys = KEYS[type_]['read']
+    query = parse_form(form, query_keys)
+    # query search with not enough parameters
+    if not query:
+        raise Exception(
+            f'Modo de leitura com parâmetros inválidos.<br>Informe um ou mais dos parâmetros: \"{", ".join(query_keys)}\"')
+    # query by ID
+    if 'id' in query:
+        id_ = query['id']
+        item = [x for x in items if x['id'] == id_]
+        if not item:
+            return f'Item com ID \"{id_}\" não encontrado'
+        return item[0]
+    # query by parameters
     return [item for item in items if any(
         [True for k, v in query.items() if item[k] == v])]
+
+
+def update(type_, form):
+    return
+
+
+def delete(type_, form):
+    return
 
 
 if __name__ == '__main__':
