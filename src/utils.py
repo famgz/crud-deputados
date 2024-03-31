0bd01f5a8c90e9be621a9c3e3c245b0f4828549d
famgz <famgz@proton.me>
2024-03-31 14:50:27 -0300

    feat: normalize function to generate email

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,19 @@
 import json
+import unicodedata
 
 
 def json_(path, data=None):
-    with open(path, encoding='utf-8') as f:
-        if data is None:
+    if data is None:
+        with open(path, 'r', encoding='utf-8') as f:
             return json.load(f)
-        json.dump(data)
+    validate = json.dumps(data)
+    with open(path, 'w', encoding='utf-8') as f:
+        json.dump(data, f, indent=2)
+
+
+def normalize(char):
+    return unicodedata.normalize('NFKD', char).encode('ascii', 'ignore').decode('ascii')
+
+
+def normalize_name(name: str):
+    return ''.join([normalize(x) for x in name if x.isalpha()]).lower()
